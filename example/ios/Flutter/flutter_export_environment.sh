84e0c6888bd958922f712da8247694d9c5a121d7
do <dujianchi512@gmail.com>
2019-10-17 22:07:30 +0800

    update version

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/do/Dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/do/du/projects/flutter/jpush-flutter-plugin/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/do/Dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
